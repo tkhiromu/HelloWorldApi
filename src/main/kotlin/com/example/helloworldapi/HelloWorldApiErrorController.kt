2f7249f9d9cf84efa3cc6ce0458adc5e4fbec9cd
tkhiromu <hiromu.takahashi@askul.com>
2024-04-09 14:58:09 +0900

    no.2-1 Exception 余計なコメント削除

--- src/main/kotlin/com/example/helloworldapi/HelloWorldApiErrorController.kt
+++ src/main/kotlin/com/example/helloworldapi/HelloWorldApiErrorController.kt
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController
 class HelloWorldApiErrorController: ErrorController {
     @RequestMapping("/error")
     fun handleError(request: HttpServletRequest): ResponseEntity<Map<String, String>> {
-        // ここでは、すべてのエラーに対して同じレスポンスを返していますが、
-        // requestから取得できる情報を使用して、エラーに応じたカスタマイズを行うことも可能です。
         val body = mapOf(
             "reason" to "no handler found",
             "status" to "404"
