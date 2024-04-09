74e29bed980f74b3acf24c60a50123005b39d388
tkhiromu <hiromu.takahashi@askul.com>
2024-04-09 09:56:34 +0900

    後学のためのコメント追加

--- src/main/kotlin/com/example/helloworldapi/ExceptionHandlerAdvice.kt
+++ src/main/kotlin/com/example/helloworldapi/ExceptionHandlerAdvice.kt
@@ -1,4 +1,4 @@
-package com.example.HelloWorldApi
+package com.example.helloworldapi
 
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -8,8 +8,24 @@ import org.springframework.http.ResponseEntity
 @RestControllerAdvice
 class ExceptionHandlerAdvice {
 
+    /**
+     * MethodArgumentNotValidException
+     * Valid アノテーションが付けられた引数の検証が失敗した場合にスローされる例外
+     * Spring Frameworkがクライアントからのリクエストのバリデーション中に、
+     * リクエストが設定されたバリデーション条件を満たさなかった場合にスローされる例外です
+     *
+     * @param ex
+     * @return
+     */
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun handleValidationExceptions(ex: MethodArgumentNotValidException): ResponseEntity<Map<String, Any>> {
+        // bindingResult
+        // データバインディングプロセス（クライアントから送られてきたデータをJavaオブジェクトにマッピングするプロセス）
+        // またはバリデーションプロセスの結果を保持します。
+        // エラーが発生した場合、これらのエラーはBindingResult内に格納され、後で検査や表示のためにアクセスできます。
+        // fieldErrors
+        // エラーが発生したフィールド名、エラーコード（バリデーションルールを表す）、デフォルトメッセージなど、エラーに関する情報が含まれます。
+
         val errors = ex.bindingResult.fieldErrors.map {
             "${it.field}: ${it.defaultMessage}"
         }
