7c23c67d5d20bc01059f13837b252b0d39d09cf1
tkhiromu <hiromu.takahashi@askul.com>
2024-04-09 10:01:24 +0900

    jsonに手で変換しないようにした jsonのコンバーター使うと、データ型そのまま返却するだけで内部でjsonにしてくれる

--- src/main/kotlin/com/example/helloworldapi/HelloWorldController.kt
+++ src/main/kotlin/com/example/helloworldapi/HelloWorldController.kt
@@ -1,8 +1,6 @@
 package com.example.helloworldapi
 
 import jakarta.validation.Valid
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -11,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 class HelloWorldController {
 
     @GetMapping("/hello")
-    fun hello(@Valid @RequestBody request: HelloWorldRequest): String {
-        return Json.encodeToString(HelloWorldResponse.responseOf(request))
+    fun hello(@Valid @RequestBody request: HelloWorldRequest): HelloWorldResponse {
+        return HelloWorldResponse.responseOf(request)
     }
 }
