941f41e4f8edd4fdf4f982d4534e4ac2241ed85e
tkhiromu <hiromu.takahashi@askul.com>
2024-04-10 09:11:33 +0900

    レビュー対応

--- src/main/kotlin/com/example/helloworldapi/HelloWorldApiErrorController.kt
+++ src/main/kotlin/com/example/helloworldapi/HelloWorldApiErrorController.kt
@@ -1,21 +1,25 @@
-package com.example.helloworldapi
+//package com.example.helloworldapi
+//
+//import jakarta.servlet.http.HttpServletRequest
+//import org.springframework.boot.web.servlet.error.ErrorController
+//import org.springframework.http.HttpStatus
+//import org.springframework.http.ResponseEntity
+//import org.springframework.web.bind.annotation.RequestMapping
+//import org.springframework.web.bind.annotation.RestController
 
-import jakarta.servlet.http.HttpServletRequest
-import org.springframework.boot.web.servlet.error.ErrorController
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-
-@RestController
-class HelloWorldApiErrorController: ErrorController {
-    @RequestMapping("/error")
-    fun handleError(request: HttpServletRequest): ResponseEntity<Map<String, String>> {
-        val body = mapOf(
-            "reason" to "no handler found",
-            "status" to "404"
-        )
-        return ResponseEntity(body, HttpStatus.NOT_FOUND)
-    }
-
-}
+/**
+ * この実装でも良いが、ExceptionHandlerAdviceに実装まとめられる
+ *
+ */
+//@RestController
+//class HelloWorldApiErrorController: ErrorController {
+//    @RequestMapping("/error")
+//    fun handleError(request: HttpServletRequest): ResponseEntity<Map<String, String>> {
+//        val body = mapOf(
+//            "reason" to "no handler found",
+//            "status" to "404"
+//        )
+//        return ResponseEntity(body, HttpStatus.NOT_FOUND)
+//    }
+//
+//}
