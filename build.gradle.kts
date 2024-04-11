74519631b8e82d6b49759fd932a4fd61e0900477
tkhiromu <hiromu.takahashi@askul.com>
2024-04-11 09:55:28 +0900

    no6 healthcheck

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
 tasks.withType<KotlinCompile> {
