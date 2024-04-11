d90b78f612e7b7ca1c59895f20c8d46d5f0988e2
tkhiromu <hiromu.takahashi@askul.com>
2024-04-11 23:07:21 +0900

    no4 レビュー対応

--- src/main/kotlin/com/example/helloworldapi/AppProperties.kt
+++ src/main/kotlin/com/example/helloworldapi/AppProperties.kt
@@ -1,10 +1,20 @@
 package com.example.helloworldapi
 
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Component
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.context.properties.bind.ConstructorBinding
+import org.springframework.context.annotation.Configuration
 
-@Component // 内部的に自動で検知されてインスタンス作られる
-class AppProperties {
-    @Value("\${text}")
-    lateinit var text: String
-}
+// lateinitはテストしずらいし使わない方がいい
+//@Component // 内部的に自動で検知されてインスタンス作られる
+//class AppProperties {
+//    @Value("\${text}")
+//    lateinit var text: String
+//}
+
+@Configuration
+@EnableConfigurationProperties(AppProperties::class)
+class AppPropertiesConfig
+
+@ConfigurationProperties(prefix = "app")
+data class AppProperties @ConstructorBinding constructor(val text: String)
--- src/main/kotlin/com/example/helloworldapi/HelloWorldController.kt
+++ src/main/kotlin/com/example/helloworldapi/HelloWorldController.kt
@@ -1,12 +1,13 @@
 package com.example.helloworldapi
 
 import jakarta.validation.Valid
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HelloWorldController(private val appProperties: AppProperties) {
+class HelloWorldController(@Autowired private val appProperties: AppProperties) {
 
     @GetMapping("/hello")
     fun hello(@Valid @RequestBody request: HelloWorldRequest): HelloWorldResponse {
